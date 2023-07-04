00803493b91566776600438e002bf865c0c70685
depreeth <dc3016@srmist.edu.in>
2023-07-04 11:07:19 +0530

    complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
         <div>
             <nav className="navbar navbar-expand-lg bg-dark py-3 shadow-sm">
                 <div className="container">
-                    <a className="navbar-brand fw-bold fs-4 text-white" href="/">LOGOHERE..</a>
+                    <a className="navbar-brand fw-bold fs-4 text-white" href="/">E-COM</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
